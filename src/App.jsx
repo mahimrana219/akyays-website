1c5d53ec6f90f6bd02998f235bd3f0dfac645532
Mahim Rana <mahimrana219@gmail.com>
2026-01-30 12:52:22 -0800

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Navbar from '../src/Components/Navbar';
 import Hero from '../src/Components/Hero';
 import Card from '../src/Components/Card';
 import Highlights from '../src/Components/Highlights';
-import HowToInstall from './Components/HowToInstall';
+import HowToInstall from '../src/Components/HowToInstall';
 import Donate from '../src/Components/Donate'
-import Reviews from './Components/Reviews';
-import Footer from './Components/Footer';
+import Reviews from '../src/Components/Reviews';
+import Footer from '../src/Components/Footer';
 
 export default function App() {
   return (
